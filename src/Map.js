fb44a2620014f80c114a99b832f0306c891e156c
Michael Romay <amerror@gmail.com>
2020-03-03 11:25:40 -0800

    changed app.js to map.js for google maps

--- src/Map.js
+++ src/Map.js
@@ -30,8 +30,30 @@ class SimpleMap extends Component {
           />
         </GoogleMapReact>
       </div>
+
+      
     );
   }
 }
 
 export default SimpleMap;
+
+// maybe for the map / on api load
+// const handleApiLoaded = (map, maps) => {
+//   // use map and maps objects
+// };
+ 
+ 
+// <GoogleMapReact
+//   bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+//   defaultCenter={this.props.center}
+//   defaultZoom={this.props.zoom}
+//   yesIWantToUseGoogleMapApiInternals
+//   onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+// >
+//   <AnyReactComponent
+//     lat={59.955413}
+//     lng={30.337844}
+//     text="My Marker"
+//   />
+// </GoogleMapReact>
