b83af7ae4ec3cf1d626709bd531362e70ae781b8
Nathan Martel <nathanmartel@gmail.com>
2020-03-02 17:01:54 -0800

    Wiring up site skeleton

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor.</p>
+                <h2>About the Developers</h2>
+                <p>Lorem ipsum dolor.</p>
+                <p>Lorem ipsum dolor.</p>
+                <p>Lorem ipsum dolor.</p>
+                <p>Lorem ipsum dolor.</p>
+            </div>
+        )
+    }
+}
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                    <p>Include:</p>
+                    <p>Google Map</p>
+                    <p>Search</p>
+                    <p>Initial pstings shown on Google Map</p>
+            </div>
+        )
+    }
+}
