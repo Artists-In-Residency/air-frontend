c5543f6e25d1d7d0971e9b7c4fc9d9c4a28dbbaf
Nathan Martel <nathanmartel@gmail.com>
2020-03-03 16:44:42 -0800

    card semantics, api started

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import request from 'superagent';
+
+export const getFavorites = async () => {
+    const URL = `${process.env.REACT_APP_DB_URL}/listings`;
+    console.log('Requesting favorites from', URL);
+    const result = await request.get(URL);
+    return result.body;
+}
+
+export const getAllResidencies = async () => {
+    const URL = `${process.env.REACT_APP_DB_URL}/listings`;
+    console.log('Requesting all residencies from', URL);
+    const result = await request.get(URL);
+    return result.body;
+}
