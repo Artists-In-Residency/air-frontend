14768192ae29a5e0805b3f1497d91af69f171910
Michael Romay <amerror@gmail.com>
2020-03-04 18:43:42 -0800

    heroku push

--- src/ResidencyCard.js
+++ src/ResidencyCard.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import './residency-card.css';
 
+// function tryThing(params) {
+
+//     let msg = (params.msg) ? params.msg : (function(){throw "error"}());
+
+//     // do stuff if everything inside `params` is defined
+// }
+
+// function imgError(image) {
+//     image.onerror = "";
+//     image.src = "http://placekitten.com/200/200";
+//     return true;}
+
+
+
 export default class ResidencyCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { imageStatus: "loading", error: false };
+      }
+    
+      handleImageLoaded() {
+        this.setState({ imageStatus: "loaded", error: false });
+      }
+    
+      handleImageError() {
+        this.setState({ imageStatus: "failed to load", error: true });
+      }
+
     render() {
         return (
             <li className='residency-card'>
                 <div className='image-container'>
-                    <img src={this.props.item.img_url} alt='pic' />
+                        <img src={this.state.error || this.props.item.img_url ? this.props.item.img_url : 'https://www.dropbox.com/s/6yctk83lzczg443/62bolt.png'} onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageError.bind(this)} alt='pic'/>
+                                {this.state.imageStatus}
+                    {/* <img src={this.props.item.img_url ? this.props.item.img_url : 'http://placekitten.com/100/100' } onError="imgError(this);" alt='pic' /> */}
                 </div>
                 <h3>{this.props.item.program_name}</h3>
                 <p>{this.props.item.description}</p>
